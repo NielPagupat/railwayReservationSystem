005276d62686523fd82e4faaed60aa059c59c059
NielPagupat <niel.pagupat.2016@gmail.com>
2023-06-11 17:03:27 +0800

    can now book ticket

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BookingController extends Controller
+{
+    public function bookTicket(Request $request){
+        $incomingFields = $request -> validate([
+            'tNo' => 'required',
+            'bookDt' => 'required',
+            'route' => 'required',
+            'source' => 'required',
+            'destination' => 'required',
+            'sched' => 'required',
+            'time' => 'required',
+            'cat' => 'required',
+            'name' => 'required',
+            'age' => 'required',
+            'sex' => 'required',
+            'address' => 'required'
+        ]);
+
+        $bookTicket = DB::select('call bookTicket(?,?,?,?,?,?,?,?,?,?,?,?)', 
+                                    array($incomingFields['tNo'],
+                                          $incomingFields['bookDt'],
+                                          $incomingFields['route'],
+                                          $incomingFields['source'],
+                                          $incomingFields['destination'],
+                                          $incomingFields['sched'],
+                                          $incomingFields['time'],
+                                          $incomingFields['cat'],
+                                          $incomingFields['name'],
+                                          $incomingFields['age'],
+                                          $incomingFields['sex'],
+                                          $incomingFields['address']));
+        return redirect('/');
+    }
+}
--- resources/js/components/FormComp.jsx
+++ resources/js/components/FormComp.jsx
@@ -1,35 +1,63 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { FormControl, FormLabel } from '@mui/material';
-
+import Box from '@mui/material/Box';
 import React from 'react'
 
 export default function FormComp() {
   return (
-    <FormControl>
-    <FormLabel>Train Number</FormLabel>
-    <TextField></TextField>
-    <FormLabel>Book date</FormLabel>
-    <TextField></TextField>
-    <FormLabel>route</FormLabel>
-    <TextField></TextField>
-    <FormLabel>category</FormLabel>
-    <TextField></TextField>
-    <FormLabel>schedule</FormLabel>
-    <TextField></TextField>
-    <FormLabel>Source</FormLabel>
-    <TextField></TextField>
-    <FormLabel>Destination</FormLabel>
-    <TextField></TextField>
-    <FormLabel>Name</FormLabel>
-    <TextField></TextField>
-    <FormLabel>Age</FormLabel>
-    <TextField></TextField>
-    <FormLabel>Sex</FormLabel>
-    <TextField></TextField>
-    <FormLabel>Address</FormLabel>
-    <TextField></TextField>
-    <Button>Submit</Button>
-    </FormControl>
+    <>
+    <h1>Book Ticket</h1>
+    <form action="/bookTicket" method='Post'>
+      <input name='tNo' type="text" placeholder='train No.'/>
+      <input name='bookDt' type='date' placeholder='bookDate'/>
+      <select name="route" id="rt">
+        <option value="0" disabled selected>route</option>
+        <option value="1">CDO-Davao</option>
+        <option value='2'>Davao-CDO</option>
+      </select>
+      <select name="source" id="source">
+        <option value="source" disabled selected>source</option>
+        <option value="CDO">CDO</option>
+        <option value="Malaybalay">Malaybalay</option>
+        <option value="Valencia">Valencia</option>
+        <option value="Quezon">Quezon</option>
+        <option value="Lorega">Lorega</option>
+        <option value="Buda">Buda</option>
+        <option value="Mintal">Mintal</option>
+        <option value="Davao">Davao</option>
+      </select>
+      <select name="destination" id="destination">
+        <option value="dest" disabled selected>destination</option>
+        <option value="CDO">CDO</option>
+        <option value="Malaybalay">Malaybalay</option>
+        <option value="Valencia">Valencia</option>
+        <option value="Quezon">Quezon</option>
+        <option value="Lorega">Lorega</option>
+        <option value="Buda">Buda</option>
+        <option value="Mintal">Mintal</option>
+        <option value="Davao">Davao</option>
+      </select>
+      <select name="sched" id="sched">
+        <option value="sched" disabled selected>schedule</option>
+        <option value="AM">AM</option>
+        <option value="PM1">PM1</option>
+        <option value="PM2">PM2</option>
+        <option value="EVE1">EVE1</option>
+        <option value="EVE2">EVE2</option>
+      </select>
+      <input name='time' type="text" placeholder='time' />
+      <select name="cat" id="cat">
+        <option value="" disabled selected>seat category</option>
+        <option value="1">AC seat</option>
+        <option value="2">Gen seat</option>
+      </select>
+      <input name='name' type="text" placeholder='name' />
+      <input name='age' type="text" placeholder='age'/>
+      <input name='sex' type="text" placeholder='sex'/>
+      <input name='address' type="text" placeholder='address' />
+      <input type="submit" value='Book Ticket' />
+    </form>
+    </>
   )
 }
